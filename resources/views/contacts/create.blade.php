c60918d054365c51ffdc58a1d3e2e6db801ed9ed
samComeIt <sanhae1996@gmail.com>
2020-09-06 16:16:02 +0900

    multiply forms one submit button

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -1,5 +1,20 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
 Create Contact!
-<form method="post" action="{{ route('Contact::store') }}" enctype="multipart/form-data">
+<?php
+
+
+?>
+<!-- @for ($i = 0; $i < 10; $i++)
+    The current value is {{ $i }}
+@endfor -->
+
+@for ($i = 0; $i < 2; $i++)
+
+<form method="post" action="{{ route('Contact::store') }}"  class="myForms" enctype="multipart/form-data">
     {!! csrf_field() !!}
 
     <td>이름:
@@ -13,6 +28,20 @@ Create Contact!
     <td>
         <label for="image">사진:</label>
         <input type="file" name="contactImage">
-    </td>
-    <button type="submit" class="xe-btn">저장</button>
+</td>
+    <!--<button type="submit" class="xe-btn">저장</button>-->
 </form>
+
+@endfor
+<button onclick="submit_forms();">SUBMIT</button>
+
+<script>
+
+    function submit_forms(){
+        var x = document.querySelectorAll(".myForms");
+        for (let i = 0; i < x.length; i++) {
+           x[i].submit();
+        }
+    }
+
+</script>
